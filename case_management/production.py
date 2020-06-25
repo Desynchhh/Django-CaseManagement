03acd27f0bb82baf79990fb3f91789da431569dc
Desynch <ltheguy939@gmail.com>
2020-06-25 10:24:19 +0200

    Changed some settings in an attempt to load custom css files x3. Now sets the DEBUG settings in production vs in development automatically.

--- case_management/production.py
+++ case_management/production.py
@@ -4,6 +4,8 @@ from .settings import *
 # Configure default domain name
 ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.azurewebsites.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
+DEBUG = False
+
 # Configure Postgres database
 DATABASES = {
     'default': {
