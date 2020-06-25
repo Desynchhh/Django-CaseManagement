dacb3b3784921c4305985cbb8b384cbecdb69345
Desynch <ltheguy939@gmail.com>
2020-06-25 11:28:41 +0200

    Fixing mimetypes attempt #6

--- case_management/settings.py
+++ case_management/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATIC_URL = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = '/static_root/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
