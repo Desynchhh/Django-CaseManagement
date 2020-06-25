f7bdf28dbc05daaa214248b8e06f9bed96e00498
Desynch <ltheguy939@gmail.com>
2020-06-25 10:43:25 +0200

    Fixing mimetyes attempt #1

--- case_management/settings.py
+++ case_management/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, mimetypes
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
